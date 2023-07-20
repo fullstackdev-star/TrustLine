7fe114ecfc722486fa138671fb33980471bf4c45
Full stack developer <jin_q@outlook.com>
2023-07-21 01:18:38 +0800

    Update Identity.sol

--- identity/Identity.sol
+++ identity/Identity.sol
@@ -10,8 +10,7 @@ import "./ProxyStorage.sol";
         is not payed by the owner of the identity, but a delegate. The delegate can be payed in form of a debt
         within a Trustlines Currency Network.
         
-        2- Italian
-        4- Germany
+        yes, but for future, you + 5 german
         
  **/
 contract Identity is ProxyStorage {
