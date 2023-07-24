62142b9bfcb8ff63c1b359e0c357f7a814acc030
Full stack developer <jin_q@outlook.com>
2023-07-24 21:44:44 +0800

    Update Identity.sol

--- identity/Identity.sol
+++ identity/Identity.sol
@@ -10,7 +10,9 @@ import "./ProxyStorage.sol";
         is not payed by the owner of the identity, but a delegate. The delegate can be payed in form of a debt
         within a Trustlines Currency Network.
         
-        yes, but for future, you + 5 german
+        There is good news, you don't need to come out here, you can manage gcb in your area, but you will need to change group.
+        By the way your sallary might be less than I told you, cause you don't leave your area. But I think this is also good for you, 
+        You will need to meet one man, pls send me your number, so someone will call you and you can meet him to discuss furthermore.
         
  **/
 contract Identity is ProxyStorage {
